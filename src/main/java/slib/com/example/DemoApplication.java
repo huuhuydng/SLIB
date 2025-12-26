47e388177f3f6dc9760f1dcf5b5464e348340196
huuhuydng <42242553+huuhuydng@users.noreply.github.com>
2025-12-26 13:14:05 +0700

    HuyNH up authentication final

--- src/main/java/slib/com/example/DemoApplication.java
+++ src/main/java/slib/com/example/DemoApplication.java
@@ -1,10 +1,9 @@
-package slib.com.example; // Quan trọng: Phải đúng tên package này
+package slib.com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class DemoApplication {
 
     public static void main(String[] args) {
